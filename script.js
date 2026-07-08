8d6011fb0bcfcbdac91c05e3b657c43cd4da88cc
akwldrk <117529582+akwldrk@users.noreply.github.com>
2026-07-08 12:08:33 +0900

    Update script.js

--- script.js
+++ script.js
@@ -164,3 +164,35 @@ window.addEventListener("scroll", () => {
     });
 
 });
+
+/* =============================
+   Back To Top Button
+============================= */
+
+const topBtn = document.getElementById("topBtn");
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 300){
+
+        topBtn.style.display = "block";
+
+    }else{
+
+        topBtn.style.display = "none";
+
+    }
+
+});
+
+topBtn.addEventListener("click", () => {
+
+    window.scrollTo({
+
+        top:0,
+
+        behavior:"smooth"
+
+    });
+
+});
